c63a61e5b016929eb736c03d14df04826a0c34ca
petziferum <daboarderpjb@gmail.com>
2023-03-02 22:21:06 +0100

    marker objekte

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -11,24 +11,12 @@
         name="Karte"
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
     ></l-tile-layer>
-    <l-control-attribution
-        position="bottomleft"
-        :prefix="customAttributionPrefix"
-    />
-    <l-control-scale position="bottomleft" />
-    <l-layer-group>
-      <l-marker :lat-lng="coordinates2" draggable>
-        <l-popup> Hi! You can drag me around! </l-popup>
-      </l-marker>
-    </l-layer-group>
-
-    <l-marker :lat-lng="coordinates" draggable>
-      <l-icon icon-url="https://placekitten.com/32/32" :icon-size="[48, 48]" />
-      <l-tooltip> What a tiny kitten </l-tooltip>
+    <l-marker v-for="(marker,i) in markers" :key="i" :lat-lng="marker.latlng" @click="removeMarker(i)" draggable>
+      <l-tooltip>{{ marker.text }}<br>{{ marker.latlng }}</l-tooltip>
     </l-marker>
-    <l-marker v-for="(marker,i) in markers" :key="i" :lat-lng="marker" @click="removeMarker(i)"></l-marker>
   </l-map>
   </div>
+  {{ markers }}
 </template>
 
 <script setup>
@@ -45,16 +33,18 @@ import {
 } from "@vue-leaflet/vue-leaflet";
 import {ref} from "vue";
 
-const center = ref([41.61322, -87.219482])
-const zoom = ref(100);
+const center = ref([48.12148528243583, 11.63177490234375])
+const zoom = ref(13);
 const coordinates = ref([41.61322, -87.219839])
 const coordinates2 = ref([41.61322, -87.219483])
 const customAttributionPrefix = ref("<strong>Custom bottom left attribution</strong>")
-const markers = ref([L.LatLng(41.61322, -87.219839), L.LatLng(41.61322, -87.219483)])
+const markers = ref([{ text: "first marker", latlng: L.latLng(48.12148528243583, 11.63177490234375)}])
 
 function addMarker(e) {
   console.log("add marker", e.latlng);
-  markers.value.push(e.latlng);
+  if(e.latlng) {
+    markers.value.push({text: "new Marker", latlng: e.latlng});
+  }
 }
 
 function removeMarker(index) {
