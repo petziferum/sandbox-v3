d3315130bd9d6219d019c43a336b52a405083116
petziferum <daboarderpjb@gmail.com>
2023-03-02 21:41:58 +0100

    marker einfügen

--- src/components/TheMap.vue
+++ src/components/TheMap.vue
@@ -4,18 +4,18 @@
       ref="map"
       v-model:zoom="zoom"
       :center="center"
+      @click="addMarker"
       >
     <l-tile-layer
         layer-type="base"
         name="Karte"
         url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
     ></l-tile-layer>
-    <l-tile-layer
-        url="https://s3.amazonaws.com/te512.safecast.org/{z}/{x}/{y}.png"
-        attribution="<a href='https://blog.safecast.org/about/'>SafeCast</a> (<a href='https://creativecommons.org/licenses/by-sa/3.0/'>CC-BY-SA</a>"
-        :min-zoom="5"
-        :max-zoom="7"
+    <l-control-attribution
+        position="bottomleft"
+        :prefix="customAttributionPrefix"
     />
+    <l-control-scale position="bottomleft" />
     <l-layer-group>
       <l-marker :lat-lng="coordinates2" draggable>
         <l-popup> Hi! You can drag me around! </l-popup>
@@ -26,6 +26,7 @@
       <l-icon icon-url="https://placekitten.com/32/32" :icon-size="[48, 48]" />
       <l-tooltip> What a tiny kitten </l-tooltip>
     </l-marker>
+    <l-marker v-for="(marker,i) in markers" :key="i" :lat-lng="marker" @click="removeMarker(i)"></l-marker>
   </l-map>
   </div>
 </template>
@@ -38,7 +39,9 @@ import {
   LMarker,
   LPopup,
   LTooltip,
-    LIcon
+    LControlAttribution,
+    LIcon,
+    LControlScale
 } from "@vue-leaflet/vue-leaflet";
 import {ref} from "vue";
 
@@ -46,9 +49,17 @@ const center = ref([41.61322, -87.219482])
 const zoom = ref(100);
 const coordinates = ref([41.61322, -87.219839])
 const coordinates2 = ref([41.61322, -87.219483])
+const customAttributionPrefix = ref("<strong>Custom bottom left attribution</strong>")
+const markers = ref([L.LatLng(41.61322, -87.219839), L.LatLng(41.61322, -87.219483)])
 
-function onMapClick(e) {
-  alert("Du hast geklickt" + e.latLng);
+function addMarker(e) {
+  console.log("add marker", e.latlng);
+  markers.value.push(e.latlng);
+}
+
+function removeMarker(index) {
+  console.log("Remove", index);
+  this.markers.splice(index, 1);
 }
 </script>
 
