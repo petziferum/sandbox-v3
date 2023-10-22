8cd1355cc181b31c24ff1c94d1e5442a6f562cd6
petziferum <daboarderpjb@gmail.com>
2023-10-22 12:11:16 +0200

    resolve undefined

--- src/components/products/types/ProductStatus.ts
+++ src/components/products/types/ProductStatus.ts
@@ -1,18 +1,19 @@
-export enum productStatus  {
+enum productStatus  {
   AVAILABLE= "AVAILABLE",
   UNAVAILABLE = "UNAVAILABLE",
   INSTOCK = "INSTOCK",
   ORDER = "ORDER"
 };
 
-export const statusToBeschreibung = new Map([
+export const statusToBeschreibung = new Map<string, string>([
     [productStatus.AVAILABLE, "Verfügbar"],
     [productStatus.UNAVAILABLE, "Nicht verfügbar"],
     [productStatus.INSTOCK, "Auf Lager"],
     [productStatus.ORDER, "In Zulieferung"]
 ])
 
-export const statusAsRecord: Record<string, string>[] = Object.values(productStatus).map((key: productStatus) => {
-    return { text: statusToBeschreibung.get(key), value: key };
+export const statusAsRecord: Record<string, string>[] = Object.values(productStatus).map((key) => {
+    return { text: statusToBeschreibung.get(key) as string, value: key };
 });
 
+export default productStatus;
