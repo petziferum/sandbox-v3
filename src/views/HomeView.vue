4bafd0c9f7bb73fa194db90199d1e7a1bbb31fed
petziferum <daboarderpjb@gmail.com>
2023-10-22 11:18:14 +0200

    Home Button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
 <template>
-
+  <v-row justify="center">
+    <v-col cols="12" md="10">
+      <v-card>
+        <v-card-title>
+          <h3>Home</h3>
+        </v-card-title>
+        <v-toolbar>
+          <v-toolbar-title>Home</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn text to="/lab">Lab</v-btn>
+        </v-toolbar>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts" setup>
