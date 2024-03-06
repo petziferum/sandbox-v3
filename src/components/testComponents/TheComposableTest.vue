952806a7ee037412f8fc41816e50627315191303
petziferum <daboarderpjb@gmail.com>
2024-03-06 22:15:14 +0100

    add vueUse

--- src/components/testComponents/TheComposableTest.vue
+++ src/components/testComponents/TheComposableTest.vue
@@ -11,6 +11,12 @@
           <v-btn @click="resetText">reset Text</v-btn>
         </v-col>
       </v-row>
+      <v-row>
+        <v-col>
+          Hier kann durch VueUse ein Composable eingebunden werden.<br>
+          <v-btn text link target="_blank" href="https://vueuse.org/guide/">Vue Use Docs</v-btn>
+        </v-col>
+      </v-row>
     </v-card-text>
   </v-card>
 </v-container>
@@ -18,6 +24,7 @@
 
 <script lang=ts setup>
 import { text, addText, resetText } from '@/components/testComponents/Composable.logic.ts';
+import { useArrayFind } from "@vueuse/core";
 </script>
 
 <style scoped>
