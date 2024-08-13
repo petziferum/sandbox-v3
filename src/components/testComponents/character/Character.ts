b0cb82dafc2081a19dbf06c4f2cffe3d4bc0d19a
petziferum <daboarderpjb@gmail.com>
2024-08-13 11:17:14 +0200

    method chaining

--- src/components/testComponents/character/Character.ts
+++ src/components/testComponents/character/Character.ts
@@ -13,8 +13,43 @@ export default class Character {
         this.charisma = charisma;
     }
 
+    withFirstName(firstName: string): Character {
+        this.firstName = firstName;
+        return this;
+    }
+
+    withLastName(lastName: string): Character {
+        this.lastName = lastName;
+        return this;
+    }
+
+    withStrength(strength: number): Character {
+        this.strength = this.validateAttribute(strength);
+        return this;
+    }
+
+    withEndurance(endurance: number): Character {
+        this.endurance = this.validateAttribute(endurance);
+        return this;
+    }
+
+    withCharisma(charisma: number): Character {
+        this.charisma = this.validateAttribute(charisma);
+        return this;
+    }
+
+    createEmptyCharacter(): Character {
+        return new Character();
+    }
+
     public addStrength(value: number): void {
         this.strength += value;
     }
 
+    private validateAttribute(value: number): number {
+        if (value < 0 || value > 999) {
+            throw new Error("Value must be between 0 and 999.");
+        }
+        return Math.round(value * 100) / 100; // Rounds to two decimal places
+    }
 }
