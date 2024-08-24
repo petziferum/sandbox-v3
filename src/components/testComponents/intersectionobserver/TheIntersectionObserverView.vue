f3a362a96ce67f4b9e8bc6a18209c791b9391835
petziferum <daboarderpjb@gmail.com>
2024-08-24 21:02:22 +0200

    intersection

--- src/components/testComponents/intersectionobserver/TheIntersectionObserverView.vue
+++ src/components/testComponents/intersectionobserver/TheIntersectionObserverView.vue
@@ -1,40 +1,50 @@
 <template>
-<v-card title="Intersection Observer with Tabs Animation">
-  <v-card-subtitle>
-    <v-tabs grow v-model="tab_selected">
-      <v-tabs-slider />
-      <span  v-for="(category, index) in tabs" :key="index">
-      <v-tab @click="$vuetify.goTo('#sec_'+index)" class="nav_tab">
-        {{ category }}
+  <v-card title="Intersection Observer with Tabs Animation">
+    <v-card-subtitle>
+      <v-tabs grow v-model="tab_selected" slider-color="red">
+        <v-tab v-for="(category, index) in categories" :key="index" class="nav_tab">
+          {{ category }}
         </v-tab>
-      </span>
-    </v-tabs>
-  </v-card-subtitle>
-  <v-card-text style="height:150px; overflow: scroll">
-    <div v-for="(category, index) in categories" :key="index">
-      <v-card :id="'sec_'+index" class="mb-10 elevation-4" color="blue" height="300" width="400">
-        <span :id="'span_'+index" v-intersect="handleIntersect"></span>
-        <strong>...{{category}}...</strong>
-      </v-card>
-    </div>
-  </v-card-text>
-</v-card>
+        <template v-slot:slider>
+          <div class="custom-tab-slider"></div>
+        </template>
+      </v-tabs>
+    </v-card-subtitle>
+    <v-card-text style="height: 250px; overflow-y: scroll">
+      <div v-for="(category, index) in categories" :key="index">
+        <v-card v-intersect="handleIntersect" :id="'sec_'+index" class="contentcard elevation-4" color="blue" height="200" width="400">
+            <strong>...{{ category }} - index: {{ index }}...</strong>
+        </v-card>
+      </div>
+    </v-card-text>
+  </v-card>
 </template>
-<script setup lang=ts>
-import {ref} from "vue";
 
-const tabs = ref(5);
-const tab_selected = ref(1);
+<script setup lang="ts">
+import {ref} from 'vue';
+
 const categories = ref(["Category 1", "Category 2", "Category 3", "Category 4", "Category 5"]);
+const tab_selected = ref("0");
 
-const handleIntersect = (entries: IntersectionObserverEntry[]) => {
-  let intersecting_element = entries[0]
-  if(intersecting_element.isIntersecting){
-    let id = intersecting_element.target.id
-    tab_selected.value = Number(id.split("_")[1])
+const handleIntersect = (isIntersecting: boolean, entries: IntersectionObserverEntry[], entry: IntersectionObserverEntry) => {
+  if (isIntersecting) {
+    const index = entries[0].target.id.split("_")[1];
+    console.log("index:", index);
+    tab_selected.value = index;
   }
-}
+};
 </script>
 <style scoped>
-
+.nav_tab{
+  cursor: pointer;
+  background-color: #2c3e50;
+  color: black;
+}
+.contentcard{
+  margin-bottom: 500px;
+}
+.v-tabs-slider {
+  height: 10px;
+  background-color: white;
+}
 </style>
