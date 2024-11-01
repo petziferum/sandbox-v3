7d4c4fb6e4d9d3e566b1da95abf4fb2c8be81199
petziferum <daboarderpjb@gmail.com>
2024-11-01 17:27:03 +0100

    anpassungen

--- src/views/LabView.vue
+++ src/views/LabView.vue
@@ -4,19 +4,19 @@
     <h4>Crazy Experiments are going on in here....</h4>
     <v-expansion-panels>
       <wrapper-panel title="Intersection Observer with Tabs Animation">
-        <the-intersection-observer-view />
+        <the-intersection-observer-view/>
       </wrapper-panel>
       <wrapper-panel title="Character Test">
-        <character-test />
+        <character-test/>
       </wrapper-panel>
       <wrapper-panel title="Map & Set Zeugnis">
-        <map-und-set />
+        <map-und-set/>
       </wrapper-panel>
       <wrapper-panel title="Composable test">
-        <the-composable-test />
+        <the-composable-test/>
       </wrapper-panel>
       <wrapper-panel title="Props and Emits">
-        <the-props-test />
+        <the-props-test/>
       </wrapper-panel>
       <wrapper-panel title="Abfahrsauskunft U2">
         <v-card>
@@ -40,45 +40,46 @@
           </v-card-text>
         </v-card>
       </wrapper-panel>
-    <wrapper-panel title="NAS Server Gradle Backend Test">
-      <v-card>
-        <v-card-title>
-          <h3>Experiment local Server Backend</h3>
-        </v-card-title>
-        <v-card-text>
-          <v-row>
-            <v-col class="text-grey">
-              Das angesprochene Backend liegt auf dem Nas Server in einem Docker Container. Dort läuft es auf Port 8080. Das Image ist auf DockerHub und heist "gradlebackend".
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="6">
-              <v-btn @click="getHelloFromGradleBackend">Hello</v-btn>
-            </v-col>
-            <v-col cols="6">
-              Antwort: "{{ serverResponse }}"
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </wrapper-panel>
-    <wrapper-panel title="local Gallery">
-        <local-gallery />
+      <wrapper-panel title="NAS Server Gradle Backend Test">
+        <v-card>
+          <v-card-title>
+            <h3>Experiment local Server Backend</h3>
+          </v-card-title>
+          <v-card-text>
+            <v-row>
+              <v-col class="text-grey">
+                Das angesprochene Backend liegt auf dem Nas Server in einem Docker Container. Dort läuft es auf Port
+                8080. Das Image ist auf DockerHub und heist "gradlebackend".
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="6">
+                <v-btn @click="getHelloFromGradleBackend">Hello</v-btn>
+              </v-col>
+              <v-col cols="6">
+                Antwort: "{{ serverResponse }}"
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
       </wrapper-panel>
-    <wrapper-panel title="Sort and Filter">
-        <the-sort-and-filter />
+      <wrapper-panel title="local Gallery">
+        <local-gallery/>
       </wrapper-panel>
-    <wrapper-panel title="The Grid">
-        <the-grid />
+      <wrapper-panel title="Sort and Filter">
+        <the-sort-and-filter/>
       </wrapper-panel>
-    <wrapper-panel title="Product View and Props">
-        <product-view />
+      <wrapper-panel title="The Grid">
+        <the-grid/>
       </wrapper-panel>
-    <wrapper-panel title="Base Dialog">
+      <wrapper-panel title="Product View and Props">
+        <product-view/>
+      </wrapper-panel>
+      <wrapper-panel title="Base Dialog">
         Dialog
-        <base-dialog />
+        <base-dialog/>
       </wrapper-panel>
-    <wrapper-panel title="Grief Password Generator">
+      <wrapper-panel title="Grief Password Generator">
         <v-card style="background-color: #2c3e50;" theme="dark">
           <v-card-title>
             Grief Password Generator
@@ -90,19 +91,19 @@
             Dein Passwort: <strong>{{ password }}</strong>
           </v-card-item>
           <v-card-text class="ml-4">
-          <div class="ma-3">Alte Passwörter:</div>
-          <ul>
-            <li v-for="old in oldPassword">{{ old }}</li>
-          </ul>
+            <div class="ma-3">Alte Passwörter:</div>
+            <ul>
+              <li v-for="old in oldPassword">{{ old }}</li>
+            </ul>
           </v-card-text>
         </v-card>
       </wrapper-panel>
-    <wrapper-panel title="Ratin Component">
+      <wrapper-panel title="Rating Component">
         Rating
         <the-movie-review :rating="1" movie-title="Ein scheiß Film"/>
       </wrapper-panel>
-    <wrapper-panel title="The Weather API">
-        <the-weather-history />
+      <wrapper-panel title="The Weather API">
+        <the-weather-history/>
       </wrapper-panel>
       <wrapper-panel title="Experiment 1">
         <v-card>
@@ -115,8 +116,8 @@
           </v-card-text>
         </v-card>
       </wrapper-panel>
-    <wrapper-panel title="Product Forms">
-        <the-product-form />
+      <wrapper-panel title="Product Forms">
+        <the-product-form/>
       </wrapper-panel>
     </v-expansion-panels>
   </v-container>
@@ -153,7 +154,7 @@ const callNames = async function () {
 
 const generatePassword = function (): void {
   console.log("leng", password.value.length);
-  if(password.value.length > 1) {
+  if (password.value.length > 1) {
     oldPassword.value.push(password.value);
   }
   console.log("old", oldPassword.value)
@@ -197,7 +198,6 @@ function passwordGenerator() {
 
   password.value = pass;
 }
-
 
 
 </script>
